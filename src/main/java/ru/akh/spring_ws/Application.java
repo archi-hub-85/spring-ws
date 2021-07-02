3ac004600ce862b808a207d9c17c541e8328f636
Artur Khubbutdinov <archi_hub@mail.ru>
2021-07-02 17:13:29 +0500

    1. Added tests for all implemented operations to BookServiceTest. 2. Added security tests (invalid user, invalid password, invalid role) to BookServiceTest. 3. Replaced SimpleSoapExceptionResolver with SoapFaultMappingExceptionResolver for Wss4jSecurityInterceptor. 4. Added separate log category "org.springframework.ws.server.endpoint.ExceptionResolverWarnings" for warnings (exceptions with stack traces) of SoapFaultMappingExceptionResolver. 5. Fixed MySpringSecurityPasswordValidationCallbackHandler - added check of successful execution of super method.

--- src/main/java/ru/akh/spring_ws/Application.java
+++ src/main/java/ru/akh/spring_ws/Application.java
@@ -2,10 +2,10 @@ package ru.akh.spring_ws;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.PostConstruct;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
 
@@ -19,6 +19,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,9 +27,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.WsSecurityValidationException;
 import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
 import org.springframework.ws.soap.security.wss4j2.callback.SpringSecurityPasswordValidationCallbackHandler;
-import org.springframework.ws.soap.server.endpoint.SimpleSoapExceptionResolver;
+import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
+import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
 
 @SpringBootApplication
 public class Application {
@@ -92,22 +95,35 @@ public class Application {
     @ConditionalOnProperty(name = "ru.akh.spring-ws.security-enabled", havingValue = "true", matchIfMissing = true)
     public static class WsSecurityConfig {
 
-        @Autowired
-        private SimpleSoapExceptionResolver simpleSoapExceptionResolver;
+        @Bean
+        public SoapFaultMappingExceptionResolver soapFaultMappingExceptionResolver() {
+            SoapFaultMappingExceptionResolver exceptionResolver = new SoapFaultMappingExceptionResolver();
 
-        @PostConstruct
-        public void init() {
-            simpleSoapExceptionResolver.setWarnLogCategory(SimpleSoapExceptionResolver.class.getName());
+            // it's the only one way to log exception with stack trace
+            exceptionResolver.setWarnLogCategory("org.springframework.ws.server.endpoint.ExceptionResolverWarnings");
+
+            SoapFaultDefinition defaultFaultDef = new SoapFaultDefinition();
+            defaultFaultDef.setFaultCode(SoapFaultDefinition.SERVER);
+            exceptionResolver.setDefaultFault(defaultFaultDef);
+
+            Properties exceptionMappings = new Properties();
+            exceptionMappings.put(WsSecurityValidationException.class.getName(), SoapFaultDefinition.CLIENT.toString());
+            exceptionMappings.put(AccessDeniedException.class.getName(),
+                    SoapFaultDefinition.CLIENT.toString() + ",Access is denied");
+            exceptionResolver.setExceptionMappings(exceptionMappings);
+
+            return exceptionResolver;
         }
 
         @Bean
         public Wss4jSecurityInterceptor wsSecurityInterceptor(
-                ObjectProvider<CallbackHandler> callbackHandlerProvider) {
+                ObjectProvider<CallbackHandler> callbackHandlerProvider,
+                SoapFaultMappingExceptionResolver exceptionResolver) {
             Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
             securityInterceptor.setValidationActions("UsernameToken");
             securityInterceptor
                     .setValidationCallbackHandlers(callbackHandlerProvider.stream().toArray(CallbackHandler[]::new));
-            securityInterceptor.setExceptionResolver(simpleSoapExceptionResolver);
+            securityInterceptor.setExceptionResolver(exceptionResolver);
             return securityInterceptor;
         }
 
@@ -133,15 +149,17 @@ public class Application {
             super.handleUsernameToken(callback);
 
             String password = callback.getPassword();
-            Matcher matcher = PASSWORD_ALGORITHM_PATTERN.matcher(password);
-            if (matcher.matches()) {
-                String encodingId = matcher.group(1);
-                logger.warn("Password contains encodingId '" + encodingId + "'");
-                if (encodingId.equals("noop")) {
-                    String cleanPassword = matcher.group(2);
-                    callback.setPassword(cleanPassword);
-                } else {
-                    throw new UnsupportedCallbackException(callback, "Cannot decode password");
+            if (password != null) {
+                Matcher matcher = PASSWORD_ALGORITHM_PATTERN.matcher(password);
+                if (matcher.matches()) {
+                    String encodingId = matcher.group(1);
+                    logger.warn("Password contains encodingId '" + encodingId + "'");
+                    if (encodingId.equals("noop")) {
+                        String cleanPassword = matcher.group(2);
+                        callback.setPassword(cleanPassword);
+                    } else {
+                        throw new UnsupportedCallbackException(callback, "Cannot decode password");
+                    }
                 }
             }
         }
--- src/test/java/ru/akh/spring_ws/ws/client/BookServiceTest.java
+++ src/test/java/ru/akh/spring_ws/ws/client/BookServiceTest.java
@@ -1,8 +1,13 @@
 package ru.akh.spring_ws.ws.client;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mockito;
+import org.springframework.ws.soap.client.SoapFaultClientException;
 
 import ru.akh.spring_ws.dto.Author;
 import ru.akh.spring_ws.dto.Book;
@@ -18,12 +23,146 @@ public class BookServiceTest extends AbstractServiceTest {
         Mockito.when(repository.get(id)).thenReturn(book);
 
         Book actualBook = client.get(id);
-        Assertions.assertEquals(book.getId(), actualBook.getId(), "book.id");
-        Assertions.assertEquals(book.getTitle(), actualBook.getTitle(), "book.title");
-        Assertions.assertEquals(book.getYear(), actualBook.getYear(), "book.year");
-        Assertions.assertNotNull(actualBook.getAuthor(), "book.author");
-        Assertions.assertEquals(book.getAuthor().getId(), actualBook.getAuthor().getId(), "author.id");
-        Assertions.assertEquals(book.getAuthor().getName(), actualBook.getAuthor().getName(), "author.name");
+        assertEquals(book, actualBook, "book.");
+    }
+
+    @Test
+    @WithWriter
+    public void testPutBook() {
+        long id = 1;
+        Book book = createBook(id, "title", 2021, 1L, "author");
+        Mockito.when(repository.put(Mockito.any())).thenReturn(id);
+
+        long result = client.put(book);
+        Assertions.assertEquals(id, result, "result");
+    }
+
+    @Test
+    @WithReader
+    public void testGetTopBooks() {
+        Book book1 = createBook(1L, "title1", 2021, 1L, "name1");
+        Book book2 = createBook(2L, "title2", 2022, 2L, "name2");
+        Mockito.when(repository.getTopBooks(Book.Field.ID, 2))
+                .thenReturn(Arrays.asList(book1, book2));
+
+        List<Book> result = client.getTopBooks(Book.Field.ID, 2);
+        Assertions.assertEquals(2, result.size(), "result.size");
+        assertEquals(book1, result.get(0), "book1.");
+        assertEquals(book2, result.get(1), "book2.");
+    }
+
+    @Test
+    @WithReader
+    public void testGetBooksByAuthor() {
+        String author = "author";
+        Book book1 = createBook(1L, "title1", 2021, 1L, author);
+        Book book2 = createBook(2L, "title2", 2022, 1L, author);
+        Mockito.when(repository.getBooksByAuthor(author))
+                .thenReturn(Arrays.asList(book1, book2));
+
+        List<Book> result = client.getBooksByAuthor(author);
+        Assertions.assertEquals(2, result.size(), "result.size");
+        assertEquals(book1, result.get(0), "book1.");
+        assertEquals(book2, result.get(1), "book2.");
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.WRONG_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetWithWrongUser() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.get(1);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.READER_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetWithWrongPassword() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.get(1);
+        });
+    }
+
+    @Test
+    @WithWriter
+    public void testGetWithWrongRole() {
+        assertThrowsSoapFault("Client", "Access is denied", () -> {
+            client.get(1);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.WRONG_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testPutWithWrongUser() {
+        assertThrowsSoapFault("Client", null, () -> {
+            Book book = createBook(1L, "title", 2020, null, "author");
+            client.put(book);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.WRITER_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testPutWithWrongPassword() {
+        assertThrowsSoapFault("Client", null, () -> {
+            Book book = createBook(1L, "title", 2020, null, "author");
+            client.put(book);
+        });
+    }
+
+    @Test
+    @WithReader
+    public void testPutWithWrongRole() {
+        assertThrowsSoapFault("Client", "Access is denied", () -> {
+            Book book = createBook(1L, "title", 2020, null, "author");
+            client.put(book);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.WRONG_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetTopBooksWithWrongUser() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.getTopBooks(Book.Field.ID, 2);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.READER_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetTopBooksWithWrongPassword() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.getTopBooks(Book.Field.ID, 2);
+        });
+    }
+
+    @Test
+    @WithWriter
+    public void testGetTopBooksWithWrongRole() {
+        assertThrowsSoapFault("Client", "Access is denied", () -> {
+            client.getTopBooks(Book.Field.ID, 2);
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.WRONG_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetBooksByAuthorWithWrongUser() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.getBooksByAuthor("author");
+        });
+    }
+
+    @Test
+    @WithUser(username = UsersConstants.READER_USERNAME, password = UsersConstants.WRONG_PASSWORD)
+    public void testGetBooksByAuthorWithWrongPassword() {
+        assertThrowsSoapFault("Client", null, () -> {
+            client.getBooksByAuthor("author");
+        });
+    }
+
+    @Test
+    @WithWriter
+    public void testGetBooksByAuthorWithWrongRole() {
+        assertThrowsSoapFault("Client", "Access is denied", () -> {
+            client.getBooksByAuthor("author");
+        });
     }
 
     private static Book createBook(Long id, String title, int year, Long authorId, String authorName) {
@@ -38,6 +177,28 @@ public class BookServiceTest extends AbstractServiceTest {
         book.setAuthor(author);
 
         return book;
+    }
+
+    private static void assertEquals(Book expected, Book actual, String messagePrefix) {
+        Assertions.assertEquals(expected.getId(), actual.getId(), messagePrefix + "id");
+        Assertions.assertEquals(expected.getTitle(), actual.getTitle(), messagePrefix + "title");
+        Assertions.assertEquals(expected.getYear(), actual.getYear(), messagePrefix + "year");
+        Assertions.assertNotNull(actual.getAuthor(), messagePrefix + "author");
+        Assertions.assertEquals(expected.getAuthor().getId(), actual.getAuthor().getId(), messagePrefix + "author.id");
+        Assertions.assertEquals(expected.getAuthor().getName(), actual.getAuthor().getName(),
+                messagePrefix + "author.name");
+    }
+
+    private void assertThrowsSoapFault(String expectedFaultCode, String expectedFaultStringOrReason,
+            Executable executable) {
+        SoapFaultClientException exception = Assertions.assertThrows(SoapFaultClientException.class, executable);
+        logger.debug("exception = {}", exception.toString());
+
+        Assertions.assertEquals(expectedFaultCode, exception.getFaultCode().getLocalPart(), "faultCode");
+        if (expectedFaultStringOrReason != null) {
+            Assertions.assertEquals(expectedFaultStringOrReason, exception.getFaultStringOrReason(),
+                    "faultStringOrReason");
+        }
     }
 
 }
